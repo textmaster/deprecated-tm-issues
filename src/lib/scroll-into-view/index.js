1077c4de5d1664308e2707c51eec117d7be2549f
Josep M Sobrepere <josepot@gmail.com>
2017-03-05 21:26:48 -0500

    Making scrolling to step smooth

--- src/lib/scroll-into-view/index.js
+++ src/lib/scroll-into-view/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import smoothScroll from 'smoothscroll';
 
 export default Component => class ScrollIntoView extends React.Component {
   render() {
@@ -9,6 +10,10 @@ export default Component => class ScrollIntoView extends React.Component {
   componentDidMount() {
     /* eslint-disable react/no-find-dom-node */
     const domNode = ReactDOM.findDOMNode(this);
-    domNode.scrollIntoView({ behavior: 'smooth' });
+    const absBottom =
+      domNode.getBoundingClientRect().bottom -
+      document.body.getBoundingClientRect().top;
+    const targetScroll = Math.max(0, absBottom - window.innerHeight);
+    smoothScroll(targetScroll);
   }
 };
