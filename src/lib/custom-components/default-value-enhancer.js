b26fda0f7fb49b873bb64a89b05b42b0ab7d8077
Josep M Sobrepere <josepot@gmail.com>
2017-03-07 09:32:23 -0500

    Making 'defaultValueEnhancer' stop propagatting 'defaultValue' prop

--- src/lib/custom-components/default-value-enhancer.js
+++ src/lib/custom-components/default-value-enhancer.js
@@ -1,9 +1,16 @@
-import { lifecycle } from 'recompose';
+import R from 'ramda';
+import { compose, lifecycle, mapProps } from 'recompose';
 
-export default lifecycle({ componentWillMount() {
-  const { input, originalProps } = this.props;
-  if (!input.value && originalProps.defaultValue !== undefined) {
-    input.onChange(originalProps.defaultValue);
-  }
-} });
+export default compose(
+  lifecycle({ componentWillMount() {
+    const { input, originalProps } = this.props;
+    if (!input.value && originalProps.defaultValue !== undefined) {
+      input.onChange(originalProps.defaultValue);
+    }
+  } }),
+  mapProps(({ originalProps, ...others }) => ({
+    originalProps: R.omit(['defaultValue'], originalProps),
+    ...others,
+  })),
+);
 
