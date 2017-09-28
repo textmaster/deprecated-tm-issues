79b868a32f0ac9573967639a225b97769b873c54
Josep M Sobrepere <josepot@gmail.com>
2017-09-28 16:25:40 +0200

    PL requests over flowdock (#5)
    * Adding babel-plugin-ramda
    
    * Adjustments required by PL over flowdock

--- src/modules/form-issue/sagas.js
+++ src/modules/form-issue/sagas.js
@@ -10,14 +10,14 @@ const goToUrl = (url) => { window.location.href = url; };
 
 function* submit(action) {
   const { payload:
-    { title, type, audience, priority, platform, description },
+    { title, type, priority, platform, description },
   } = action;
   const token = yield select(tokenSelector);
   const targetRepo = yield select(targetRepoSelector);
 
   const { type: actionType, payload: { html_url: issueUrl } } = yield call(
     requestSequence,
-    [submitIssue, token, targetRepo, title, type, audience, priority, platform, description],
+    [submitIssue, token, targetRepo, title, type, priority, platform, description],
     'form-issue',
     action,
   );
